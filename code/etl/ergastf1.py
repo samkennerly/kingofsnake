a46bae6b3ed3de68ea5be04067d09af0671a8991
Sam Kennerly <samkennerly@gmail.com>
2019-12-11 19:03:17 -0500

    Update etl.ergastf1

--- code/etl/ergastf1.py
+++ code/etl/ergastf1.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+from itertools import chain, starmap
 from json import dump, load
 from operator import itemgetter
 from pathlib import Path
@@ -29,14 +31,32 @@ class ErgastF1:
 
         return f"{name}({params})"
 
+    def __getitem__(self, query):
+        return self.read(query) if self.folder else self.pages(query)
+
     # Generic queries
 
-    def get(self, query, limit=LIMIT, offset=0):
+    def ondisk(self, query):
+        """ List[Path]: Paths to saved query replies (if any). """
+        return sorted((self.folder / str(query)).glob('*.json'))
+
+    def pages(self, query, limit=LIMIT):
+        """ Iterator[Dict]: Raw replies to query. """
+        queried = self.queried
+
+        offset, total = 0, 1
+        while offset < total:
+            page = queried(query, limit=limit, offset=offset)
+            total = int(page['MRData']['total'])
+            offset += limit
+            yield page
+
+    def queried(self, query, limit=LIMIT, offset=0):
         """ dict: JSON response to query. Retries automatically. """
         retries, timeout = self.retries, self.timeout
 
         url = f"{BASE}/{query}.json?limit={limit}&offset={offset}"
-        for itry in reversed(range(retries)):
+        for trials in reversed(range(retries)):
             try:
                 print(f"GET {url}")
                 reply = getrequest(url, timeout=timeout)
@@ -45,37 +65,25 @@ class ErgastF1:
                 return reply.json()
 
             except Exception as err:
-                if itry:
+                if trials:
                     alert(err)
-                    print(f"Retry in {timeout} seconds ({itry} left)")
+                    print(f"Retry in {timeout} seconds ({trials} left)")
                     sleep(timeout)
                     timeout *= 2
                 else:
                     raise err
 
-    def pages(self, query, limit=LIMIT):
-        """ Iterator[Dict]: Raw pages returned by query. """
-        get = self.get
-
-        offset, total = 0, 1
-        while offset < total:
-            page = get(query, limit=limit, offset=offset)
-            total = int(page['MRData']['total'])
-            offset += limit
-            yield page
-
-    @classmethod
-    def queries(cls):
-        raise NotImplementedError
-
     def read(self, query):
-        raise NotImplementedError
+        """ Iterator[dict]: Saved query replies. """
+        ondisk, save = self.ondisk, self.save
+
+        paths = ondisk(query) or save(query) or ondisk(query)
+        for path in paths:
+            with open(path) as file:
+                yield load(file)
 
     def save(self, query, limit=LIMIT, **kwargs):
-        """
-        None: Save query response pages as JSON files in a folder.
-        Assign paths automatically. Delete existing JSON files in folder.
-        """
+        """ None: Get query replies and save to JSON files. """
         folder, pages = self.folder, self.pages
 
         kwargs.setdefault("allow_nan", False)
@@ -99,14 +107,81 @@ class ErgastF1:
                 print("save", path)
                 dump(page, file, **kwargs)
 
+    # Unpackers
+
+    @staticmethod
+    def tupled(rows, name, *cols):
+        """ Iterator[namedtuple]: Rows with selected column names. """
+        return starmap(namedtuple(name, cols), map(itemgetter(*cols), rows))
+
+    def unpacked(self, query, *keys):
+        """ Iterator: Row dictionaries extracted from query replies. """
+        rows = self[query]
+        for key in keys:
+            rows = map(itemgetter(key), rows)
+
+        return chain.from_iterable(rows)
+
     # Specific queries
 
+    def circuits(self):
+        tupled, unpacked = self.tupled, self.unpacked
+
+        cols = 'circuitId circuitName country lat long locality url'.split()
+        rows = unpacked('circuits', 'MRData', 'CircuitTable', 'Circuits')
+        rows = ( {**x, **x.pop('Location')} for x in rows )
+
+        return tupled(rows, 'Circuit', *cols)
+
+    def constructors(self):
+        tupled, unpacked = self.tupled, self.unpacked
+
+        cols = 'constructorId name nationality url'.split()
+        rows = unpacked('constructors', 'MRData', 'ConstructorTable', 'Constructors')
+
+        return tupled(rows, 'Constructor', *cols)
+
+    def drivers(self):
+        tupled, unpacked = self.tupled, self.unpacked
+
+        cols = 'driverId dateOfBirth familyName givenName nationality url'.split()
+        rows = unpacked('drivers', 'MRData', 'DriverTable', 'Drivers')
+
+        return tupled(rows, 'Driver', *cols)
+
+    def races(self):
+        seasons, tupled, unpacked = self.seasons, self.tupled, self.unpacked
+
+        cols = 'circuitId date raceName round season url'.split()
+        rows = ( unpacked(x, 'MRData', 'RaceTable', 'Races') for x in seasons() )
+        rows = chain.from_iterable(rows)
+        rows = ( (x, x.pop('Circuit').pop('circuitId')) for x in rows )
+        rows = ( {**x, **{'circuitId': y}} for x, y in rows )
+
+        return tupled(rows, 'Race', *cols)
+
     def seasons(self):
+        unpacked = self.unpacked
 
-        data = page('seasons').pop('MRData').pop('SeasonTable').pop('Seasons')
-        data = map(itemgetter('season'), data)
+        rows = self.unpacked('seasons', 'MRData', 'SeasonTable', 'Seasons')
+        rows = map(itemgetter('season'), rows)
 
-        return tuple(map(int, data))
+        return sorted(rows)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
