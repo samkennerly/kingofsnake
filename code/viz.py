8d50c34e50ed3717f3268ef031cf372778438ea9
Sam Kennerly <samkennerly@gmail.com>
2019-12-22 22:36:37 -0500

    Simplify viz.py

--- code/viz.py
+++ code/viz.py
@@ -7,69 +7,61 @@ from pandas import DataFrame
 
 AXES = (
     ('frame_on', False),
-    ('xlabel', None),
-    ('ylabel', None),
 )
-COLORMAP = 'nipy_spectral_r'
 FIGURE = (
     ('clear', True),
     ('dpi', 100),
+    ('edgecolor', None),
+    ('facecolor', None),
     ('figsize', (9, 5)),
+    ('frameon', False),
+    ('tight_layout', True),
 )
 LEGEND = (
     ('bbox_to_anchor', (1.05, 1)),
     ('borderaxespad', 0.0),
     ('loc', 'upper left'),
 )
-DEFAULT = (
-    ('figsize', (10, 5)),
-    ('fontsize', None),
-    ('grid', True),
-    ('legend', True),
-    ('logx', False),
-    ('logy', False),
-    ('rot', 0),
-    ('style', None),
-    ('title', None),
-    ('use_index', True),
-    ('xlim', None),
-    ('xticks', None),
-    ('ylim', None),
-    ('yticks', None),
-)
 
 class Plot:
     """
-    Plotting methods for pandas.DataFrame or .Series inputs.
-    Initialize with defaults. Call with keyword arguments to override defaults.
+    Plot maker for pandas.DataFrame or .Series inputs.
     """
     styles = mpstyle.available
 
     def __init__(self, style='bmh', **kwargs):
-        self.default = {**dict(DEFAULT), **kwargs}
         mpstyle.use(style)
 
-    def __call__(self, data, **kwargs):
-        """ AxesSubplot: Fill default values and create figure. """
-        kwargs = {**self.default, **kwargs}
+        params = dict()
+        params['axes'] = { k:kwargs.pop(k,v) for k,v in AXES }
+        params['figure'] = { k:kwargs.pop(k,v) for k,v in FIGURE }
+        params['legend'] = { k:kwargs.pop(k,v) for k,v in LEGEND }
 
-        if 'color' in kwargs:
-            kwargs.pop('colormap', None)
+        self.params = params
+
+    def __call__(self, data, **kwargs):
+        axes, params = self.axes, self.params
+
+        kwargs.setdefault('grid', False)
+        kwargs.setdefault('legend', False)
+        xlabel = kwargs.pop('xlabel', None)
+        ylabel = kwargs.pop('ylabel', None)
 
         data = DataFrame(data)
-        fig = figure(**{ k:kwargs.pop(k, v) for k, v in FIGURE })
-        ax = fig.add_subplot(**{ k:kwargs.pop(k, v) for k, v in AXES })
-        ax = data.plot(ax=ax, **kwargs)
+        axes = kwargs.pop('ax', None) or axes()
+        axes = data.plot(ax=axes, **kwargs)
+        axes.set_xlabel(xlabel)
+        axes.set_ylabel(ylabel)
         if kwargs.get('legend'):
-            ax.legend(**dict(LEGEND))
+            axes.legend(**params['legend'])
 
-        return ax
+        return axes
 
     def __repr__(self):
-        name = type(self).__name__
-        params = ", ".join( f"{k}={v}" for k,v in vars(self).items() )
+        return f"{type(self).__name__}({self.params})"
 
-        return f"{name}({params})"
+    def axes(self):
+        return figure(**self.params['figure']).add_subplot(**self.params['axes'])
 
     # DataFrame input
 
@@ -79,7 +71,7 @@ class Plot:
 
     def bar(self, data, **kwargs):
         """ AxesSubplot: Bar plot for each column. """
-        kwargs.setdefault('grid', False)
+        kwargs.setdefault('legend', True)
         kwargs.setdefault('rot', 90)
         kwargs.setdefault('stacked', True)
         kwargs.setdefault('width', 0.9)
@@ -88,7 +80,7 @@ class Plot:
 
     def barh(self, data, **kwargs):
         """ AxesSubplot: Horizontal bar plot for each column. """
-        kwargs.setdefault('grid', False)
+        kwargs.setdefault('legend', True)
         kwargs.setdefault('stacked', True)
         kwargs.setdefault('width', 0.8)
 
@@ -96,8 +88,6 @@ class Plot:
 
     def box(self, data, **kwargs):
         """ AxesSubplot: Box plot for each column. """
-        kwargs.setdefault('color', None)
-        kwargs.setdefault('legend', False)
         kwargs.setdefault('grid', True)
         kwargs.setdefault('rot', 90)
 
@@ -105,13 +95,15 @@ class Plot:
 
     def boxh(self, data, **kwargs):
         """ AxesSubplot: Horizontal box plot for each column. """
-        kwargs.setdefault('vert', False)
-        kwargs.setdefault('rot', 0)
+        kwargs.setdefault('grid', True)
 
-        return self.box(data[reversed(data.columns)], **kwargs)
+        return self(data, kind='box', vert=False, **kwargs)
 
     def density(self, data, **kwargs):
         """ AxesSubplot: Probability density estimate for each column. """
+        kwargs.setdefault('grid', True)
+        kwargs.setdefault('legend', True)
+
         return self(data, kind='density', **kwargs)
 
     def heat(self, data, **kwargs):
@@ -125,28 +117,38 @@ class Plot:
     def hist(self, data, **kwargs):
         """ AxesSubplot: Histogram for each column. """
         kwargs.setdefault('bins', 33)
+        kwargs.setdefault('grid', True)
+        kwargs.setdefault('legend', True)
         kwargs.setdefault('stacked', True)
 
         return self(data, kind='hist', **kwargs)
 
     def line(self, data, **kwargs):
         """ AxesSubplot: Line plot for each column. """
+        kwargs.setdefault('grid', True)
+        kwargs.setdefault('legend', True)
+
         return self(data, kind='line', **kwargs)
 
     def scatter(self, data, **kwargs):
         """
         AxesSubplot: Scatterplot with first 2 columns as (x,y) pairs.
-        If 3rd column exists, then its values are colors for each point.
-        If 4th column exists, then its values are sizes for each point.
+        If 3rd column exists, then its values are point colors.
+        If 4th column exists, then its values are point sizes.
         """
+        data = DataFrame(data)
         cols = data.columns
         kwargs.setdefault('alpha', .707)
-        kwargs.setdefault('colormap', COLORMAP)
+        kwargs.setdefault('colorbar', False)
+        kwargs.setdefault('colormap', 'nipy_spectral_r')
+        kwargs.setdefault('grid', True)
         kwargs.setdefault('legend', False)
         kwargs.setdefault('x', cols[0])
         kwargs.setdefault('y', cols[1])
-        kwargs.setdefault('c', data[cols[2]] if len(cols) > 2 else 'black')
-        kwargs.setdefault('s', data[cols[3]] if len(cols) > 3 else 64)
+        if len(cols) > 2:
+            kwargs.setdefault('c', data[cols[2]])
+        if len(cols) > 3:
+            kwargs.setdefault('s', data[cols[3]])
 
         return self(data, kind='scatter', **kwargs)
 
@@ -155,6 +157,8 @@ class Plot:
     def quant(self, ts, freq, q=(), **kwargs):
         """ AxesSubplot: Contour plot of quantiles per period. """
         kwargs.setdefault('color', list('krygbck'))
+        kwargs.setdefault('grid', True)
+        kwargs.setdefault('legend', True)
         kwargs.setdefault('stacked', False)
 
         q = list(q) or [0, 0.05, 0.25, 0.50, 0.75, 0.95, 1]
