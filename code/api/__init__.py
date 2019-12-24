7b28f3de8c17ea3dafbe0b1b804cd4e19e70148a
Sam Kennerly <samkennerly@gmail.com>
2019-12-24 18:23:51 -0500

    Simplify code imports

--- code/api/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Application Programming Interface tools.
-"""
-from pathlib import Path
-
-from .ergast import ErgastAPI
-
-REPO = Path(__file__).resolve().parent.parent.parent
-CACHE = REPO / "data/cache"
--- code/tools.py
+++ code/tools.py
@@ -1,21 +1,18 @@
 """
-Extract, transform, and load raw data.
+Constants and utility functions.
 """
 from pathlib import Path
 
 from pandas import DataFrame
 
-from .ergast import ErgastF1
-
-REPO = Path(__file__).resolve().parent.parent.parent
+REPO = Path(__file__).resolve().parent.parent
 DATADIR = REPO / "data"
-
+F1_CACHE = DATADIR / "ergast/api"
+F1_ZIPPED = DATADIR / "ergast/f1.zip"
 
 def afew(data, n=5):
     """ DataFrame: Print DataFrame metadata and return a few rows. """
-    schema = DataFrame()
-    schema["dtype"] = data.dtypes
-    schema["nulls"] = data.isnull().sum()
+    schema = DataFrame({"dtype": data.dtypes, "nulls": data.isnull().sum()})
     print(f"[{len(data)} rows x {len(schema)} columns]", schema, sep="\n")
 
     return data.tail(n)
