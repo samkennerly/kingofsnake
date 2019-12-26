cec62eb7884e1a026c9b390e480bb414861e2877
Sam Kennerly <samkennerly@gmail.com>
2019-12-26 18:27:39 -0500

    Update viz/graph.py

--- code/viz/graph.py
+++ code/viz/graph.py
@@ -1,24 +1,28 @@
+from numpy import fromiter, linspace, tanh
+from numpy.random import randn
 from pandas import Categorical, DataFrame
-from scipy.sparse import coo_matrix
+from scipy.sparse import coo_matrix, diags, identity
+from scipy.sparse.csgraph import laplacian
 
-LINKS = (
-    ('dave', 'dave'),
-    ('dave', 'eve'),
-    ('alice', 'bob'),
-    ('bob', 'carol'),
-    ('carol', 'alice'),
-)
+def limited(z, maxr):
+    """ ndarray: Array with magnitudes smoothly compressed to <= 1. """
+    rad = abs(z).clip(1e-9, None)
+    rad = tanh(rad) / rad
+    rad *= maxr
+
+    return rad * z
 
 def weighted(links):
     """ DataFrame: [source, target, weight] for each link. """
     links = DataFrame(links)
     cols = list(links.columns)
+
     links = links.groupby(cols[0:2], observed=True)
     links = links[cols[2]].sum() if (len(cols) > 2) else links.size().rename("_")
-
-    nodes = links.index.unique(level=0) | links.index.unique(level=1)
     links = links.loc[links.ne(0)].astype(float).reset_index()
     links.columns = 'source target weight'.split()
+
+    nodes = sorted(set(links['source'].unique()) | set(links['target'].unique()))
     for col in 'source target'.split():
         links[col] = Categorical(links[col], categories=nodes)
 
@@ -33,36 +37,63 @@ class Graph:
     Source and target are categorials with the same categories.
     """
 
-    def __init__(self, links):
-        self.frame = links.frame if isinstance(links, type(self)) else weighted(links)
+    def __init__(self, graph):
+        self.links = graph.links if isinstance(graph, type(self)) else weighted(graph)
+
+    def __call__(self, steps=100):
+        matrix, nodes = self.matrix, self.nodes
+
+        n = len(nodes)
+        dtype = 'complex128'
+        points = 0.5 * (randn(n) + 1j * randn(n)).astype(dtype)
+
+        yield points.real.copy(), points.imag.copy()
+
+        matrix -= diags(matrix.diagonal())
+        matrix *= -n / matrix.sum()
+        matrix = laplacian(matrix, use_out_degree=True)
+        matrix -= identity(n, dtype=matrix.dtype, format=matrix.format)
+
+        for speed in linspace(1, 0.1, steps-1):
+            forces = ( z - points for z in points )
+            forces = ( z / (z * z.conj()).real.clip(1e-3, None) for z in forces )
+            forces = fromiter(( z.mean() for z in forces ), count=n, dtype=dtype)
+            forces += matrix.dot(points)
+            points += limited(forces, speed)
+
+            yield points.real.copy(), points.imag.copy()
+
+    def __iter__(self):
+        return self.links.itertuples(index=False, name='Link')
 
     def __len__(self):
-        return len(self.frame)
+        return len(self.links)
 
     def __repr__(self):
-        return f"{type(self).__name__} with {len(self)} links\n{self.frame}"
+        return f"{type(self).__name__} with {len(self)} links\n{self.links}"
 
-    @property
-    def links(self):
-        """ Iterator[namedtuple]: (source, target, weight) for each link. """
-        return self.frame.itertuples(index=False, name='Link')
+    def frame(self, n):
+        nodes = self.nodes
+        for x,y in self(n): pass
+
+        return DataFrame({'x': x, 'y': y}, index=nodes)
 
     @property
     def matrix(self):
         """ scipy.sparse.coo: Links as a sparse matrix. """
-        frame, nodes = self.frame, self.nodes
+        links, nodes = self.links, self.nodes
 
-        i = frame['source'].cat.codes.values
-        j = frame['target'].cat.codes.values
-        k = frame['weight'].values
+        i = links['source'].cat.codes.values
+        j = links['target'].cat.codes.values
+        k = links['weight'].values
         n = len(nodes)
 
-        return coo_matrix((k, (i, j)), shape=(n, n))
+        return coo_matrix((k, (i, j)), shape=(n, n)).tocsr()
 
     @property
     def nodes(self):
         """ Index: Sorted union of sources and targets. """
-        return self.frame['source'].cat.categories
+        return self.links['source'].cat.categories
 
 
 
