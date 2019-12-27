9880ebb9eb75fee5b1388ad2078bb3aafd073538
Sam Kennerly <samkennerly@gmail.com>
2019-12-26 21:40:31 -0500

    Autoformat with Black

--- code/viz/graph.py
+++ code/viz/graph.py
@@ -4,6 +4,7 @@ from pandas import Categorical, DataFrame
 from scipy.sparse import coo_matrix, diags, identity
 from scipy.sparse.csgraph import laplacian
 
+
 def limited(z, maxr):
     """ ndarray: Array with magnitudes smoothly compressed to <= 1. """
     rad = abs(z).clip(1e-9, None)
@@ -11,6 +12,7 @@ def limited(z, maxr):
     rad *= maxr
 
     return rad * z
+
 
 def weighted(links):
     """ DataFrame: [source, target, weight] for each link. """
@@ -20,13 +22,14 @@ def weighted(links):
     links = links.groupby(cols[0:2], observed=True)
     links = links[cols[2]].sum() if (len(cols) > 2) else links.size().rename("_")
     links = links.loc[links.ne(0)].reset_index()
-    links.columns = 'source target weight'.split()
+    links.columns = "source target weight".split()
 
-    nodes = sorted(set(links['source'].unique()) | set(links['target'].unique()))
-    for col in 'source target'.split():
+    nodes = sorted(set(links["source"].unique()) | set(links["target"].unique()))
+    for col in "source target".split():
         links[col] = Categorical(links[col], categories=nodes)
 
     return links
+
 
 class Graph:
     """
@@ -45,7 +48,7 @@ class Graph:
         matrix, nodes = self.matrix, self.nodes
 
         n = len(nodes)
-        dtype = 'complex128'
+        dtype = "complex128"
         points = 0.5 * (randn(n) + 1j * randn(n)).astype(dtype)
 
         yield points.real.copy(), points.imag.copy()
@@ -55,17 +58,17 @@ class Graph:
         matrix = laplacian(matrix, use_out_degree=True)
         matrix -= identity(n, dtype=matrix.dtype, format=matrix.format)
 
-        for speed in linspace(1, 0.1, steps-1):
-            forces = ( z - points for z in points )
-            forces = ( z / (z * z.conj()).real.clip(1e-3, None) for z in forces )
-            forces = fromiter(( z.mean() for z in forces ), count=n, dtype=dtype)
+        for speed in linspace(1, 0.1, steps - 1):
+            forces = (z - points for z in points)
+            forces = (z / (z * z.conj()).real.clip(1e-3, None) for z in forces)
+            forces = fromiter((z.mean() for z in forces), count=n, dtype=dtype)
             forces += matrix.dot(points)
             points += limited(forces, speed)
 
             yield points.real.copy(), points.imag.copy()
 
     def __iter__(self):
-        return self.links.itertuples(index=False, name='Link')
+        return self.links.itertuples(index=False, name="Link")
 
     def __len__(self):
         return len(self.links)
@@ -77,19 +80,19 @@ class Graph:
         """ DataFrame: [node|x,y] after n steps of evolution. """
         nodes = self.nodes
 
-        for x,y in self(n):
+        for x, y in self(n):
             pass
 
-        return DataFrame({'x': x, 'y': y}, index=nodes)
+        return DataFrame({"x": x, "y": y}, index=nodes)
 
     @property
     def matrix(self):
         """ scipy.sparse.coo: Links as a sparse matrix. """
         links, nodes = self.links, self.nodes
 
-        i = links['source'].cat.codes.values
-        j = links['target'].cat.codes.values
-        k = links['weight'].values
+        i = links["source"].cat.codes.values
+        j = links["target"].cat.codes.values
+        k = links["weight"].values
         n = len(nodes)
 
         return coo_matrix((k, (i, j)), shape=(n, n)).tocsr()
@@ -97,20 +100,4 @@ class Graph:
     @property
     def nodes(self):
         """ Index: Sorted union of sources and targets. """
-        return self.links['source'].cat.categories
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return self.links["source"].cat.categories
