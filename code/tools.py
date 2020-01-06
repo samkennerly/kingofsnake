95cc58af90f80f6280d0a289fe4fdeb761e389c6
Sam Kennerly <samkennerly@gmail.com>
2020-01-06 14:43:29 -0500

    Add tools.zscores()

--- code/tools.py
+++ code/tools.py
@@ -9,8 +9,10 @@ from sklearn.datasets import load_iris
 from graph import Graph
 from plot import Plot
 
+
 REPO = Path(__file__).resolve().parent.parent
 DATADIR = REPO / "data"
+
 
 def afew(data, n=5):
     """ DataFrame: Print DataFrame metadata and return a few rows. """
@@ -19,6 +21,7 @@ def afew(data, n=5):
 
     return data.tail(n)
 
+
 def iris():
     """ DataFrame: Fisher's iris dataset with covfefe removed. """
     data = load_iris()
@@ -26,5 +29,15 @@ def iris():
     cats = Categorical.from_codes(data.target, data.target_names)
     data = DataFrame(data.data, columns=cols)
     data.insert(0, 'species', cats)
+
+    return data
+
+
+def zscores(data, robust=False):
+    """ DataFrame: Data with each column standardized. """
+    data = DataFrame(data).copy()
+    for col in data.columns:
+        data[col] -= data[col].median() if robust else data[col].mean()
+        data[col] /= data[col].mad() if robust else data[col].std()
 
     return data
