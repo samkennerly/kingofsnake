ac126a4d403e5b5f1c636bee08b2590995007d23
Sam Kennerly <samkennerly@gmail.com>
2023-11-09 19:11:40 -0600

    Refactor tools.py

--- code/tools.py
+++ code/tools.py
@@ -4,7 +4,7 @@ Constants and convenience functions.
 from pathlib import Path
 
 from numpy import random
-from pandas import Categorical, DataFrame, Series, crosstab, read_csv, to_datetime
+from pandas import Categorical, DataFrame, Series, read_csv, to_datetime
 from sklearn.datasets import load_iris
 
 
@@ -14,7 +14,7 @@ DATADIR = REPO / "data"
 
 def afew(data, n=5):
     """ DataFrame view: Distinct random rows from a DataFrame. """
-    return data.loc[random.choice(data.index, size=n, replace=False)]
+    return data.loc[random.choice(data.index, size=n, replace=False)].copy()
 
 
 def datasplit(data, nrows=0):
@@ -29,19 +29,19 @@ def datasplit(data, nrows=0):
 
 def irisdata():
     """ DataFrame: Fisher's iris dataset from scikit-learn. """
-    data = load_iris()
-    cols = [x.rstrip("(cm)").strip().replace(" ", "_") for x in data.feature_names]
-    cats = Categorical.from_codes(data.target, data.target_names)
+    raw = load_iris()
+    cols = [x.rstrip("(cm)").strip().replace(" ", "_") for x in raw.feature_names]
+    cats = Categorical.from_codes(raw.target, raw.target_names)
+    data = DataFrame(raw.data, columns=cols).assign(species=cats)
 
-    return DataFrame(data.data, columns=cols).assign(species=cats)
+    return data
 
 
 def zscores(data, robust=False):
-    """ DataFrame: Data with each column standardized. """
+    """ DataFrame: Data with each numeric column standardized. """
     data = DataFrame(data).copy()
-    cols = data.select_dtypes('number').columns
 
-    for c in cols:
+    for c in data.select_dtypes('number').columns:
         data[c] -= (data[c].median() if robust else data[c].mean())
         data[c] /= (data[c].abs().mean() if robust else data[c].std())
 
