48bc1cfe1eccc5c6ae16258fe2c7cdd5a020bc5e
Sam Kennerly <samkennerly@gmail.com>
2023-11-20 17:55:54 -0600

    Remove copyright boilerplate

--- code/tools.py
+++ code/tools.py
@@ -46,18 +46,3 @@ def zscores(data, robust=False):
         data[c] /= data[c].abs().mean() if robust else data[c].std()
 
     return data
-
-
-# Copyright © 2023 Sam Kennerly
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
